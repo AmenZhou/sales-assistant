ae17efcc6be9a879e166d655fc264144e6becbd5
Wu <superbatironmans5@gmail.com>
2015-02-08 20:07:45 +0800

    add zh-CN files

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,9 +1,13 @@
-require 'yelp'
+# require 'yelp'
 
-Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_KEY']
-  config.consumer_secret = ENV['YELP_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
-end
+# Yelp.client.configure do |config|
+#   config.consumer_key = ENV['']
+#   # config.consumer_key = ENV['YELP_KEY']
+#   # config.consumer_secret = ENV['YELP_SECRET']
+#   config.consumer_secret = ENV['']
+#   # config.token = ENV['YELP_TOKEN']
+#   config.token = ENV['']
+#   # config.token_secret = ENV['YELP_TOKEN_SECRET']
+#   config.token_secret = ENV['']
+# end
 
