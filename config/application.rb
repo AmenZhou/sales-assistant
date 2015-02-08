9906cc6b056bf60ee8c9dc44addb421340c8dbf5
AmenZhou <chou.amen@gmail.com>
2015-02-08 15:31:35 +0000

    local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,11 @@ module SalesAssistant
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+        if key == Rails.env
+          value.each do |key, value|
+            ENV[key.to_s] = value
+          end
+        end
       end if File.exists?(env_file)
     end
   end
