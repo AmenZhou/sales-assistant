119220a8c1776d3d8625a91c3780782160b014a0
amenzhou <chou.amen@gmail.com>
2015-05-20 17:39:47 +0000

    set role for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,17 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :yelp_grabs, dependent: :destroy
 
+  before_save :set_role
+
+  enum role: [:admin, :normal]
+
   def admin?
     role == 'admin'
+  end
+
+  private
+
+  def set_role
+    role = 'normal' if role.nil?
   end
 end
